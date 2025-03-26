fc2a9d4894bf02d2d3a6604d2214b722ef250cd7
eun <xdfs0214@naver.com>
2025-03-26 17:09:49 +0900

    naver | 바로가기 기능 진행중

---
+++ main.js
@@ -0,0 +1,9 @@
+const shortcut = document.querySelector(".shortcut")
+console.log(shortcut);
+shortcut.addEventListener("click", showLayer);
+
+function showLayer(){
+    console.log("showlayer 실행됨");
+    const layer1 = document.querySelector("#layer1");
+    
+}
