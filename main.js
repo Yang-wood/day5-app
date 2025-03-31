2e378f2dbfda97eb7ba2134db5c99fbd6f5ffb68
eun <xdfs0214@naver.com>
2025-03-31 17:06:02 +0900

    네이버 (수정)
    1. 검색창
    
    2. body, footer

--- main.js
+++ main.js
@@ -21,3 +21,16 @@ goBtn.addEventListener("click", goSomeWhere);
 function goSomeWhere(){
     location.href = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com%2F";
 }
+
+// 폼 검색을 요청하면 (=검색어를 입력하고, 검색 버튼을 누를떄)
+
+const searchForm = document.querySelector("#search_form")
+
+searchForm.addEventListener ("click", getResult);
+
+function getResult(e){
+    e.preventDefault();
+    fetch('http://localhost:3000/news')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
